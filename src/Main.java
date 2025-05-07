f6dcbc59759a624c717f4a17e8064d3700964162
Maria Luiza <marialuizavitoria81@gmail.com>
2025-05-06 21:21:15 -0300

    Refactor: reestruturando as pastas

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,7 @@ public class Main {
         Pessoa2 pessoa2 = new Pessoa2("leticia",98,"feminino","45678907654");
         Aluno2 alunu = new Aluno2("23456789Ha", "ads", "Leticioo", 13, "masculino", "34567890765");
 
-
         System.out.println(alunu.getNome() + "é maior de idade? " + alunu.maioridade());
-
-
-
+        System.out.println("alguma coisa pra ver");
     }
 }
